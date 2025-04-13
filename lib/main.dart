027e7b00b8544d9dab007992570974fcb3b93dfe
andriimobdev25 <andriivynokurov0@gmail.com>
2025-04-13 17:13:52 +0200

    Update token management and Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:linkedin_writer/app/core/config/theme.dart';
 import 'package:linkedin_writer/app/core/services/openai_service.dart';
 import 'package:linkedin_writer/app/features/presentation/blocs/chat/chat_bloc.dart';
 import 'package:linkedin_writer/app/features/presentation/screens/chat_screen.dart';
+import 'package:linkedin_writer/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
